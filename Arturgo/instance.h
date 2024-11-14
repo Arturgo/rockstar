484311049ab040b5cd656d520c9c33e98aa667c8
Arturgo <arturgo@hotmail.fr>
2024-11-14 16:05:22 +0100

    Calcul du score !

--- Arturgo/instance.h
+++ Arturgo/instance.h
@@ -17,7 +17,22 @@ struct Ordre {
 };
 
 struct Station {
-    string name;
+    string name; int Delta;
+};
+
+struct Partition {
+    int iStation, cost;
+    vector<int> partition;
+};
+
+struct RollingWindow {
+    int iStation, cost, window_size, maximum;
+    vector<bool> is_in;
+};
+
+struct Batch {
+    int iStation, cost, minimum, maximum;
+    vector<bool> is_in;
 };
 
 struct Solution {
@@ -30,18 +45,35 @@ struct Solution {
 };
 
 struct Instance {
-    int delta;
+    int delta, rcost;
     vector<Station> stations;
     vector<bool> two_tones;
+    vector<Partition> partitions;
+    vector<RollingWindow> windows;
+    vector<Batch> batches;
+
+    int id_station(string name);
 
     int nb_vehicules() { return two_tones.size(); }
     vector<int> paint_reorder(const vector<int>& p);
+
+    int resequencing_cost(const vector<int>& input, const vector<int>& output, int Delta);
+    int partition_cost(const vector<int>& partition, const vector<int>& input);
+    int window_cost(const vector<bool>& is_in, int size, int maximum, const vector<int>& input);
+    int batch_cost(const vector<bool>& is_in, int minimum, int maximum, const vector<int>& input);
 
     Instance(json data);
     Solution solve();
 };
 
 /* Logique des fonctions */
+
+int Instance::id_station(string name) {
+    for(int i = 0;i < (int)stations.size();i++) {
+        if(name == stations[i].name) return i;
+    }
+    return -1;
+}
 
 vector<int> Instance::paint_reorder(const vector<int>& p) {
     list<int> elems(p.begin(), p.end());
@@ -73,9 +105,24 @@ vector<int> Instance::paint_reorder(const vector<int>& p) {
     return pp;
 }
 
+int Instance::resequencing_cost(const vector<int>& input, const vector<int>& output, int Delta) {
+    vector<int> pos_in(nb_vehicules()), pos_out(nb_vehicules());
+
+    for(int i = 0;i < nb_vehicules();i++) {
+        pos_in[input[i]] = i;
+        pos_out[output[i]] = i;
+    }
+
+    int total = 0;
+    for(int i = 0;i < nb_vehicules();i++) {
+        total += max(0, pos_in[i] - Delta - pos_out[i]);
+    }
+    return total;
+}
+
 Instance::Instance(json data) {
     for(json shop : data["shops"]) {
-        stations.push_back({(string)shop["name"]});
+        stations.push_back({(string)shop["name"], (int)shop["resequencing_lag"]});
     }
 
     two_tones.resize(data["vehicles"].size());
@@ -84,6 +131,56 @@ Instance::Instance(json data) {
     }
 
     delta = (int)data["parameters"]["two_tone_delta"];
+    rcost = (int)data["parameters"]["resequencing_cost"];
+
+    for(json constraint : data["constraints"]) {
+        string type = constraint["type"];
+        if(type == "lot_change") {
+            Partition cstr;
+            cstr.iStation = id_station(constraint["shop"]);
+            cstr.cost = constraint["cost"];
+
+            vector<int> partition(nb_vehicules(), 0);
+            int iPart = 0;
+            for(json part : constraint["partition"]) {
+                for(int vid : part) {
+                    partition[vid - 1] = iPart;
+                }
+                iPart++;
+            }
+
+            cstr.partition = partition;
+            partitions.push_back(cstr);
+        } else if(type == "rolling_window") {
+            RollingWindow cstr;
+            cstr.iStation = id_station(constraint["shop"]);
+            cstr.cost = constraint["cost"];
+            cstr.window_size = constraint["window_size"];
+            cstr.maximum = constraint["max_vehicles"];
+
+            vector<bool> is_in(nb_vehicules(), false);
+            for(int vid : constraint["vehicles"]) {
+                is_in[vid - 1] = true;
+            }
+            cstr.is_in = is_in;
+
+            windows.push_back(cstr);
+        } else if(type == "batch_size") {
+            Batch cstr;
+            cstr.iStation = id_station(constraint["shop"]);
+            cstr.cost = constraint["cost"];
+            cstr.minimum = constraint["min_vehicles"];
+            cstr.maximum = constraint["max_vehicles"];
+
+            vector<bool> is_in(nb_vehicules(), false);
+            for(int vid : constraint["vehicles"]) {
+                is_in[vid - 1] = true;
+            }
+            cstr.is_in = is_in;
+
+            batches.push_back(cstr);
+        }
+    }
 }
 
 Solution Instance::solve() {
@@ -122,11 +219,82 @@ json Solution::to_json() {
     return data;
 }
 
+int Instance::partition_cost(const vector<int>& partition, const vector<int>& input) {
+    int total = 0;
+    for(int i = 0;i < nb_vehicules() - 1;i++) {
+        if(partition[input[i]] != partition[input[i + 1]]) total++;
+    }
+    return total;
+}
+
+int Instance::window_cost(const vector<bool>& is_in, int size, int maximum, const vector<int>& input) {
+    int total = 0;
+    int nb_in = 0;
+
+    int fin = 0;
+    for(;fin < size;fin++) {
+        nb_in += is_in[input[fin]];
+    }
+
+    while(true) {
+        int m = max(0, nb_in - maximum);
+        total += m * m;
+
+        if(fin >= nb_vehicules()) break;
+        nb_in += is_in[input[fin]];
+        nb_in -= is_in[input[fin - size]];
+        fin++;
+    }
+
+    return total;
+}
+
+int Instance::batch_cost(const vector<bool>& is_in, int minimum, int maximum, const vector<int>& input) {
+    int total = 0;
+    int cons = 0;
+
+    for(int vid : input) {
+        if(is_in[vid]) {
+            cons += 1;
+        } else {
+            if(cons > 0) {
+                int m = 0;
+                m = max(m, minimum - cons);
+                m = max(m, cons - maximum);
+                total += m * m;
+            }
+            cons = 0;
+        }
+    }
+
+    if(cons > 0) {
+        int m = 0;
+        m = max(m, minimum - cons);
+        m = max(m, cons - maximum);
+        total += m * m;
+    }
+
+    return total;
+}
+
 double Solution::score() {
     double total = 0.;
 
-    /* TODO : Calcule le score de la solution */
-    /* TODOEND */
+    for(int iStation = 0;iStation < (int)inst->stations.size() - 1;iStation++) {
+        total += inst->rcost * inst->resequencing_cost(ordres[iStation].exit, ordres[iStation + 1].entry, inst->stations[iStation].Delta);
+    }
+
+    for(const Partition& part : inst->partitions) {
+        total += part.cost * inst->partition_cost(part.partition, ordres[part.iStation].entry);
+    }
+
+    for(const RollingWindow& window : inst->windows) {
+        total += window.cost * inst->window_cost(window.is_in, window.window_size, window.maximum, ordres[window.iStation].entry);
+    }
+
+    for(const Batch& batch : inst->batches) {
+        total += batch.cost * inst->batch_cost(batch.is_in, batch.minimum, batch.maximum, ordres[batch.iStation].entry);
+    }
 
     return total;
 }
