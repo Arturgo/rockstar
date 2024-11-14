c93ce5ba4612b27c83273d40592ba654e3101182
Quentin Rataud <quentin.rataud@ssi.gouv.fr>
2024-11-14 17:20:08 +0100

    poustouflan: steal arthur code

--- Poustouflan/solver.cpp
+++ Poustouflan/solver.cpp
@@ -2,61 +2,114 @@
 #include <algorithm>
 #include <random>
 #include <iostream>
+#include <list>
 
-std::vector<int> paint_perm(std::vector<int>& input_perm, const Instance& instance)
-{
-    // input_perm is destroyed 
+vector<int> paint_reorder(const vector<int>& p, const Instance& instance) {
+    list<int> elems(p.begin(), p.end());
+    vector<bool> seen(instance.n, false);
 
-    int n = instance.n;
-    int delta = instance.two_tone_delta;
+    vector<int> pp;
 
-    std::vector<int> result;
-    std::vector<int> remaining_paints;
-    for (int i = 0; i < n; i++)
-    {
-        remaining_paints.push_back(
-            instance.vehicles[i].two_tone ? 2 : 1
-        );
+    auto itInsert = elems.begin(); 
+    for(int _ = 0;_ <= instance.two_tone_delta;_++) {
+        if(itInsert != elems.end())
+            itInsert++;
     }
 
-    int front = 0;
+    while(!elems.empty()) {
+        int val = *elems.begin();
+        elems.erase(elems.begin());
 
-    while (front < n)
-    {
-        int v = input_perm[front];
-        if (--remaining_paints[v] > 0)
-        {
-            for (int i = front; i < front + delta && i < n - 1; i++)
-                std::swap(input_perm[i], input_perm[i+1]);
+        if(seen[val] || instance.vehicles[val].two_tone) {
+            pp.push_back(val);
+
+            if(itInsert != elems.end())
+                itInsert++;
+        } else {
+            seen[val] = true;
+            elems.insert(itInsert , val);
         }
-        else
-            result.push_back(input_perm[front++]);
     }
-    return result;
 
+    return pp;
 }
 
 
-Solution solve(const Instance& instance) {
-    Solution result;
-    int n = instance.vehicles.size();
+Solution dumb_solution(const Instance& inst) {
+    Solution sol;
+    vector<int> id(inst.n);
+    iota(id.begin(), id.end(), 0);
+    vector<int> pid = paint_reorder(id, inst);
 
-    std::vector<int> permutation;
-    for (int i = 0; i < n; i++)
-        permutation.push_back(i);
+    sol.shop_results["body"].entry = id;
+    sol.shop_results["body"].exit = id;
+    sol.shop_results["paint"].entry = id;
+    sol.shop_results["paint"].exit = pid;
+    sol.shop_results["assembly"].entry = pid;
+    sol.shop_results["assembly"].exit = pid;
+    return sol;
+}
 
-    auto rng = std::default_random_engine {};
-    std::shuffle(permutation.begin(), permutation.end(), rng);
+Solution solve(const Instance& inst) {
+    Solution best = dumb_solution(inst);
+    double best_score = best.compute_score(inst);
 
-    for (const Shop& shop : instance.shops)
-    {
-        result.shop_results[shop.name].entry = permutation;
-        if (shop.name == "paint")
-            permutation = paint_perm(permutation, instance);
-        result.shop_results[shop.name].exit = permutation;
+    vector<vector<int>> inputs = {
+        best.shop_results["body"].entry,
+        best.shop_results["paint"].entry,
+        best.shop_results["assembly"].entry
+    };
+
+    double T = 1e3;
+    int i = 0;
+    while(T >= 1e-6) {
+        vector<vector<int>> old_inputs = inputs;
+
+        if(rand() % 2 == 0) {
+            int station = rand() % 3;
+            int id1 = rand() % inst.n;
+            int id2 = rand() % inst.n;
+            swap(inputs[station][id1], inputs[station][id2]);
+        } else {
+            int id1 = rand() % inst.n;
+            int id2 = rand() % inst.n;
+
+            int pos1, pos2;
+            for(int iStation = 0;iStation < 3;iStation++) {
+                for(int iVehicle = 0;iVehicle < inst.n;iVehicle++) {
+                    if(inputs[iStation][iVehicle] == id1) {
+                        pos1 = iVehicle;
+                    }
+                    if(inputs[iStation][iVehicle] == id2) {
+                        pos2 = iVehicle;
+                    }
+                }
+                swap(inputs[iStation][pos1], inputs[iStation][pos2]);
+            }
+        }
+
+        Solution sol;
+        sol.shop_results["body"].entry = inputs[0];
+        sol.shop_results["body"].exit = inputs[0];
+        sol.shop_results["paint"].entry = inputs[1];
+        sol.shop_results["paint"].exit = paint_reorder(inputs[1], inst);
+        sol.shop_results["assembly"].entry = inputs[2];
+        sol.shop_results["assembly"].exit = inputs[2];
+
+        double score = sol.compute_score(inst);
+        double D = best_score - score;
+
+        if(D > 0 || rand() / (double)RAND_MAX <= exp(D / T)) {
+            best_score = score;
+            best = sol;
+        } else {
+            inputs = old_inputs;
+        }
+        i++;
+        if(i % 100 == 0) T *= 0.999;
+
+        if(i % 100 == 0) cerr << i << " " << T << " " << best_score << endl;
     }
 
-    std::cerr << result.compute_score(instance) << std::endl;
-
-    return result;
+    return best;
 }
